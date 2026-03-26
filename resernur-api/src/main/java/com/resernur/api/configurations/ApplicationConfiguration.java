42fe8e31fe534d19dfa622a4af51978fd7e09296
CrimsomDuke <grottordrynor@gmail.com>
2026-03-25 22:25:10 -0400

    Iniciando proyecto. Comenzando por la API

---
+++ resernur-api/src/main/java/com/resernur/api/configurations/ApplicationConfiguration.java
@@ -0,0 +1,18 @@
+﻿package com.resernur.api.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    public org.springframework.web.servlet.config.annotation.WebMvcConfigurer webMvcConfigurer() {
+        return new org.springframework.web.servlet.config.annotation.WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(org.springframework.web.servlet.config.annotation.ViewControllerRegistry registry) {
+                registry.addRedirectViewController("/", "/api/");
+            }
+        };
+    }
+}
---
+++ resernur-api/src/main/java/com/resernur/api/configurations/CorsConfiguration.java
@@ -0,0 +1,37 @@
+﻿package com.resernur.api.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${frontend.port}")
+    private String frontendPort;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Apply to all paths
+                        .allowedOriginPatterns(
+                                "http://localhost:*",
+                                "http://127.0.0.1:*"
+                        ) // Specify allowed origins
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+
+}
