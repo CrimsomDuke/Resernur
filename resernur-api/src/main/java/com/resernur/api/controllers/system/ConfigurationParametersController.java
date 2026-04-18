9b9383717006e9d4495dfe53dc8f058ec7577bb8
CrimsomDuke <grottordrynor@gmail.com>
2026-04-18 18:53:28 -0400

    configuration parameters controller

---
+++ resernur-api/src/main/java/com/resernur/api/controllers/system/ConfigurationParametersController.java
@@ -0,0 +1,55 @@
+﻿package com.resernur.api.controllers.system;
+
+import com.resernur.api.dtos.pojos.StandardResult;
+import com.resernur.api.utils.components.config_parameters.ConfigurationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class ConfigurationParametersController {
+
+    @Autowired
+    private  ConfigurationProvider configurationProvider;
+
+    public ResponseEntity<StandardResult<String>> getParameterString(String key) {
+        try {
+            String value = configurationProvider.getString(key);
+            StandardResult<String> res =  new StandardResult<>();
+            res.setData(value);
+            return ResponseEntity.ok(res);
+        } catch (Exception ex) {
+            StandardResult<String> res =  new StandardResult<>();
+            res.setSuccess(false);
+            res.setErrorMessage(ex.getMessage());
+            return ResponseEntity.badRequest().body(res);
+        }
+    }
+
+
+    public ResponseEntity<StandardResult<Integer>> getParameterInt(String key) {
+        try {
+            Integer value = configurationProvider.getInt(key);
+            StandardResult<Integer> res =  new StandardResult<>();
+            res.setData(value);
+            return ResponseEntity.ok(res);
+        } catch (Exception ex) {
+            StandardResult<Integer> res =  new StandardResult<>();
+            res.setSuccess(false);
+            res.setErrorMessage(ex.getMessage());
+            return ResponseEntity.badRequest().body(res);
+        }
+    }
+
+    public ResponseEntity<Map<String, String>> setParameter(String key, String value){
+        try {
+            configurationProvider.saveParam(key, value);
+            return ResponseEntity.ok(Map.of("message", "Parameter updated successfully"));
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body(Map.of("error", ex.getMessage()));
+        }
+    }
+
+}
